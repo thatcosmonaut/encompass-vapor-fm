1f997d2f376cc687ed23aefa79b156af5d580b93
Evan Hemsley <2342303+ehemsley@users.noreply.github.com>
2019-05-10 02:43:12 -0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 echo "building app..."
 npm run build
 echo "deploying app to production server..."
-scp -r ./dist cosmonaut@168.235.77.138:/var/www/html
+rsync -a ./dist/ cosmonaut@168.235.77.138:/var/www/html
 echo "done!"
